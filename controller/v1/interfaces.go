202351ae64a65daa03706b737466a723925a285d
Rahul Tripathi <rahultripathidev@gmail.com>
2024-03-24 08:09:28 +0530

    ft: update interfaces

--- controller/v1/interfaces.go
+++ controller/v1/interfaces.go
@@ -4,8 +4,19 @@ import (
 	"context"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/rahul0tripathi/framecoiner/entity"
 )
 
 type AccountService interface {
 	GetTradingAccount(ctx context.Context, address common.Address) (string, error)
+	PlaceTradeRequest(
+		ctx context.Context,
+		address common.Address,
+		tokenAddress common.Address,
+		ethIn string,
+	) error
+	LatestTrade(
+		ctx context.Context,
+		address common.Address,
+	) (*entity.Trade, error)
 }
--- services/interfaces.go
+++ services/interfaces.go
@@ -1,7 +1,33 @@
 package services
 
-import "context"
+import (
+	"context"
+	"math/big"
 
-type Storage interface {
-	Read(ctx context.Context, key string) (string, error)
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/rahul0tripathi/framecoiner/entity"
+)
+
+type keyManager interface {
+	SigningAddress(ctx context.Context, owner common.Address) (common.Address, error)
+	SignTx(
+		ctx context.Context,
+		owner common.Address,
+		transaction *types.Transaction,
+		chainID *big.Int,
+	) (*types.Transaction, error)
+}
+
+type quoter interface {
+	GetQuote(ctx context.Context, token common.Address, ethIn string) (*entity.Quote, error)
+}
+
+type tradesRepo interface {
+	LatestTrade(ctx context.Context, owner common.Address) (*entity.Trade, error)
+	UpdateTrade(ctx context.Context, owner common.Address, trade *entity.Trade) error
+}
+
+type tradeProcessor interface {
+	Submit(ctx context.Context, job *entity.TradeRequest) error
 }
