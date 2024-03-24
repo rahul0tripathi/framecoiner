0e74aac8a2624d2584617ec88737fbb827437c6d
Rahul Tripathi <rahultripathidev@gmail.com>
2024-03-24 14:40:23 +0530

    ft: add zero hash

--- entity/quote.go
+++ entity/quote.go
@@ -1,5 +1,11 @@
 package entity
 
+import "github.com/ethereum/go-ethereum/common"
+
+var (
+	ZeroHash = common.HexToHash("")
+)
+
 type Quote struct {
 	To                string `json:"to"`
 	Value             string `json:"value"`
