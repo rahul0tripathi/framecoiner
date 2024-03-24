611fa1f4ac69303865a52b7ee70e579bd9eeed12
Rahul Tripathi <rahultripathidev@gmail.com>
2024-03-24 08:09:33 +0530

    ft: update errors

--- entity/errors.go
+++ entity/errors.go
@@ -6,4 +6,7 @@ var (
 	ErrEmpty = errors.New("nil value")
 
 	ErrNoAccountFound = errors.New("no account found")
+	ErrNoTradesFound  = errors.New("no trades found")
+
+	ErrNoQuoteFound = errors.New("no quote found")
 )
